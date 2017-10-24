7d1d8c2f9364e3411333c8907145453b0fcd3d4e
Ian <ian.chan96@hotmail.com>
2017-10-23 17:47:09 -0700

    replace file

--- A1/glsl/armadillo.fs.glsl
+++
@@ -1,9 +0,0 @@
-// Create shared variable. The value is given as the interpolation between normals computed in the vertex shader
-varying vec3 interpolatedNormal;
-
-/* HINT: YOU WILL NEED MORE SHARED/UNIFORM VARIABLES TO COLOR ACCORDING TO COS(ANGLE) */
-
-void main() {
-  // Set final rendered color according to the surface normal
-  gl_FragColor = vec4(normalize(interpolatedNormal), 1.0); // REPLACE ME
-}
